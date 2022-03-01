2d8d132104bac486f39e076c96a936b9117e78e4
theskidster <theskidster@gmail.com>
2022-03-01 13:40:53 -0600

    finished second demo

--- Demo2/src/org/xjge/demo2/main/Main.java
+++ Demo2/src/org/xjge/demo2/main/Main.java
@@ -1,5 +1,7 @@
 package org.xjge.demo2.main;
 
+import static org.lwjgl.glfw.GLFW.GLFW_CURSOR;
+import static org.lwjgl.glfw.GLFW.GLFW_CURSOR_HIDDEN;
 import org.xjge.core.Game;
 import org.xjge.core.Window;
 import org.xjge.core.XJGE;
@@ -16,8 +18,25 @@ public class Main {
         
         XJGE.init("/org/xjge/demo2/assets/", "org.xjge.demo2.scenes.", null);
         
+        /*
+        Make sure the image you pass to the setIcon method is located in your
+        assets folder!
+        */
+        
         Window.setIcon("img_window_icon.png");
         Window.setTitle("Demo 2");
+        Window.setDimensions(640, 480);
+        
+        /*
+        Altering the size of the window may cause it to become off center.
+        We can fix this with the following method call;
+        */
+        Window.setPositionCentered();
+        
+        //Window.setMonitor("next");
+        //Window.setFullscreen(true);
+        
+        Window.setInputMode(GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
         
         Game.setScene(new TestScene("test"));
         
--- Demo2/src/org/xjge/demo2/scenes/TestScene.java
+++ Demo2/src/org/xjge/demo2/scenes/TestScene.java
@@ -2,7 +2,9 @@ package org.xjge.demo2.scenes;
 
 import java.util.Map;
 import org.xjge.core.Camera;
+import org.xjge.core.Game;
 import org.xjge.core.Scene;
+import org.xjge.core.Window;
 import org.xjge.graphics.GLProgram;
 
 //Created: Mar 1, 2022
@@ -11,13 +13,17 @@ import org.xjge.graphics.GLProgram;
  * @author J Hoffman
  */
 public class TestScene extends Scene {
-
+    
     public TestScene(String name) {
         super(name);
     }
 
     @Override
     public void update(double targetDelta, double trueDelta) {
+        //Executes every second.
+        if(Game.tick(60)) {
+            Window.setPosition(Window.getPositionX() + 5, Window.getPositionY());
+        }
     }
 
     @Override
