9db1b92087617e14404ae5c277e1e3d0794ebfbd
theskidster <theskidster@gmail.com>
2022-02-28 20:55:37 -0600

    completed first demo

--- Demo1/src/demo1/main/Main.java
+++ Demo1/src/demo1/main/Main.java
@@ -1,9 +1,10 @@
 package demo1.main;
 
-// Created: Feb 23, 2022
-
+import demo1.scenes.TestScene;
+import org.xjge.core.Game;
 import org.xjge.core.XJGE;
 
+//Created: Feb 23, 2022
 
 /**
  * @author J Hoffman
@@ -12,7 +13,10 @@ public class Main {
 
     public static void main(String args[]) {
         
-        XJGE.init("/demo1/assets/", "demo1.scenes.", null);
+        //XJGE.init("/demo1/assets/", "demo1.scenes.", null);
+        XJGE.init("/demo1/assets/", "demo1.scenes.", null, false, true, true, false, false);
+        
+        Game.setScene(new TestScene("test")); //Set the starting scene with the Game class.
         
         XJGE.start();
         
