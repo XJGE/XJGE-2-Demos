04e9d7b7cc327bffb8ea9c3ed2cbbc8311fc8bab
theskidster <theskidster@gmail.com>
2022-03-01 20:37:35 -0600

    added some example code to Demo3- might still change some things

--- Demo3/src/org/xjge/demo3/main/Main.java
+++ Demo3/src/org/xjge/demo3/main/Main.java
@@ -1,6 +1,11 @@
 package org.xjge.demo3.main;
 
+import java.util.Map;
 import org.xjge.core.Game;
+import org.xjge.core.Hardware;
+import org.xjge.core.Logger;
+import org.xjge.core.Monitor;
+import org.xjge.core.Speaker;
 import org.xjge.core.XJGE;
 import org.xjge.demo3.scenes.TestScene;
 
@@ -14,6 +19,25 @@ public class Main {
     public static void main(String args[]) {
         
         XJGE.init("/org/xjge/demo3/assets/", "org.xjge.demo3.scenes.", null);
+        
+        Map<Integer, Monitor> monitors = Hardware.findMonitors();
+        Map<Integer, Speaker> speakers = Hardware.findSpeakers();
+        
+        monitors.values().forEach(monitor -> {
+            Logger.logInfo(monitor.name + ", " + monitor.getInfo());
+        });
+        
+        System.out.println(); //Added to make console ouput more legible
+        
+        speakers.values().forEach(speaker -> {
+            Logger.logInfo(speaker.name);
+        });
+        
+        System.out.println();
+        
+        Logger.logInfo("Vsync enabled: " + Hardware.getVSyncEnabled());
+        
+        System.out.println();
         
         Game.setScene(new TestScene("test"));
         
