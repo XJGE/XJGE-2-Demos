0b431c9ba143525ea53c73763bf4622dc07c776b
theskidster <theskidster@gmail.com>
2022-03-08 16:12:09 -0600

    added code for demo5 including simple shaders

---
+++ Demo5/src/org/xjge/demo5/assets/exampleFragment.glsl
@@ -0,0 +1,11 @@
+#version 330 core
+
+in vec3 ioColor;
+
+out vec4 ioResult;
+
+void main() {
+
+    ioResult = vec4(ioColor, 0);
+    
+}
--- Demo5/src/org/xjge/demo5/main/Main.java
+++ Demo5/src/org/xjge/demo5/main/Main.java
@@ -1,8 +1,13 @@
 package org.xjge.demo5.main;
 
+import java.util.LinkedList;
+import static org.lwjgl.opengl.GL20.*;
 import org.xjge.core.Game;
 import org.xjge.core.XJGE;
 import org.xjge.demo5.scenes.TestScene;
+import org.xjge.graphics.BufferType;
+import org.xjge.graphics.GLProgram;
+import org.xjge.graphics.Shader;
 
 /**
  * Mar 8, 2022
@@ -17,8 +22,24 @@ public class Main {
         
         XJGE.init("/org/xjge/demo5/assets/", "org.xjge.demo5.scenes.", null);
         
-        Game.setScene(new TestScene("test"));
+        //Initialize our example shader program.
+        {
+            var shaderSourceFiles = new LinkedList<Shader>() {{
+                add(new Shader("exampleVertex.glsl",   GL_VERTEX_SHADER));
+                add(new Shader("exampleFragment.glsl", GL_FRAGMENT_SHADER));
+            }};
+            
+            GLProgram exampleProgram = new GLProgram(shaderSourceFiles, "example");
+            
+            exampleProgram.use();
+            exampleProgram.addUniform(BufferType.MAT4, "uModel");
+            exampleProgram.addUniform(BufferType.MAT4, "uView");
+            exampleProgram.addUniform(BufferType.MAT4, "uProjection");
+            
+            XJGE.addGLProgram("example", exampleProgram);
+        }
         
+        Game.setScene(new TestScene("test"));
         XJGE.start();
         
     }
