fb55b149b42e3ee258f278fcba256d4387e7c1f7
theskidster <theskidster@gmail.com>
2022-06-18 11:11:28 -0500

    created demo7 project

---
+++ Demo7/src/org/xjge/demo7/main/Main.java
@@ -0,0 +1,16 @@
+package org.xjge.demo7.main;
+
+//Created: Jun 18, 2022
+
+/**
+ * @author J Hoffman
+ */
+public class Main {
+
+    public static void main(String args[]) {
+        
+        
+        
+    }
+    
+}
